3fe55b7aa960f3ece552abf7f898ee30e3c25d0f
OlegSirbu <olegsirbu2@gmail.com>
2015-11-03 18:10:07 +0200

    refactor code

--- js/app.js
+++ js/app.js
@@ -1,9 +1,5 @@
-var app = angular.module('myApp', ['ngRoute']);
-
-app.controller('ListCtrl', ListCtrl)
-  .controller('DetailCtrl', DetailCtrl);
-
-app.config(['$routeProvider', '$locationProvider',
+angular.module('myApp', ['ngRoute'])
+.config(['$routeProvider', '$locationProvider',
   function($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
@@ -16,44 +12,3 @@ app.config(['$routeProvider', '$locationProvider',
       });
   }
 ]);
-
-function ListCtrl($scope, $route, $location, MoviesServices) {
-  var cb; //cb = callback object
-  cb = {
-    success: function (data) {
-      $scope.movies = data;
-    }, 
-    failure: function () {}
-  };
-  
-  $scope.showDetail = function (article) {
-    $location.path(article.id);
-  }
-
-  //getting movies for render
-  if($route.movies){
-    $scope.movies = $route.movies;
-  }else {
-    MoviesServices.getMovies(cb);
-  }
-  
-}
-
-function DetailCtrl($scope, $location, MoviesServices) {
-  var id, cb;
-  $scope.back = function () {
-    $location.path('/');
-  };
-
-  cb = {
-    success: function (article) {
-      $scope.article = article;
-    },
-    failure: function(data) {}
-  };
-
-  id = $location.url().split('/')[1];
-  if(!!id){
-    MoviesServices.getMovie(cb, id);  
-  }
-}
